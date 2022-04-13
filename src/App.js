60b5d6772acadaa6a9831ec356bb2235ab43a143
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-04-13 14:23:22 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BookForm from './components/BookForm'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className='app'>
+    <NavBar/>
+    <main>
+    <div className='container'>
+    <BookForm/>
+    </div>
+    </main>
+   <Footer/>
+
+
+    </div>
+  )
+}
+
+export default App
